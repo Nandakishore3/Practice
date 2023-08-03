582fbee1fda0bbb94a88db17af5550ee776c5184
unknown <nandakishore.1234@gmail.com>
2023-08-03 11:16:00 +0530

    first checkin

---
+++ Project1/src/mypackage/MyClass.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		int n=1000,i;
+	    for(i=1;i<=n;i++)
+	    {
+	        if(n%i==0)
+	        System.out.println("\"%d\\t\",i");
+	
+	    }
+
+	}
+}
